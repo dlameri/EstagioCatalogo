76d5e7c577738030792361b1f73b2a19a23e3343
Uila Almeida <uila.silva@ideais.com.br>
2014-01-19 18:07:35 -0200

    novo ambiente de desenvolvimento

---
+++ catalogo/WebContent/index.jsp
@@ -0,0 +1,4 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="s" uri="/struts-tags" %>
+
+<s:action name="home" executeResult="true" namespace="/"/>
---
+++ catalogo/src/main/java/com/ideaiseletronics/web/model/Product.java
@@ -0,0 +1,23 @@
+package com.ideaiseletronics.web.model;
+
+public class Product {
+	
+	private Long idProduct;
+	private String name;
+	
+	public Product(Long idProduct, String name) {
+		this.idProduct = idProduct;
+		this.name = name;
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+	
+	public Long getIdProduct(){
+		return this.idProduct;
+	}
+	
+	
+
+}
