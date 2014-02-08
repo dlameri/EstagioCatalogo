d01b924ef6d823b99b88006efb94579604fe5e9e
RGSantos <ryan.santos@ideais.com.br>
2014-02-08 13:58:38 -0200

    Update1 ProductDetails

--- catalogo/src/main/java/com/ideaiseletronics/web/actions/ProductAction.java
+++ catalogo/src/main/java/com/ideaiseletronics/web/actions/ProductAction.java
@@ -1,15 +1,19 @@
 package com.ideaiseletronics.web.actions;
 
+import com.ideaiseletronics.web.domain.Product;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class ProductAction extends ActionSupport{
 
 	private static final long serialVersionUID = 1L;
 	
+	private static int id;
+	private Product product = new Product();
+	
 	public String showProductDetails(){
 		for (Product productSearch : listProducts) {
-			if (id == productSearch.getIdProduct()) {
-				product = productSearch;
+			if (id == productSearch.getId()) {
+				this.product = productSearch;
 				break;
 			}
 		}
