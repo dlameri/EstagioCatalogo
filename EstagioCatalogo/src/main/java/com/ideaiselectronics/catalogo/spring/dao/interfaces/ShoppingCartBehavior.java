99481ffd209178c9f1b268bc67fa13307a1998c5
UilaAlmeida <uila.silva@ideais.com.br>
2014-04-19 19:33:43 -0300

    removendo pasta

--- EstagioCatalogo/src/main/java/com/ideaiselectronics/catalogo/spring/dao/interfaces/ShoppingCartBehavior.java
+++
@@ -1,9 +0,0 @@
-package com.ideaiselectronics.catalogo.spring.dao.interfaces;
-
-public interface ShoppingCartBehavior {
-	
-	public boolean addToCart(Long itemId);
-	
-	public int cartQtd();
-
-}
--- EstagioCatalogo/src/test/java/test/unitary/RestClientTest.java
+++
@@ -1,5 +0,0 @@
-package test.unitary;
-
-public class RestClientTest {
-
-}
