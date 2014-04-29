224798dc5bede6a5a56cf65e69b957c6332408ba
EstagioX <uila.silva@ideais.com.br>
2014-04-29 15:50:33 -0300

    renomeando arquivo de configuracao da aplicacao

--- catalogo/src/main/java/com/ideaiselectronics/catalogo/util/AppConfig.java
+++ catalogo/src/main/java/com/ideaiselectronics/catalogo/util/AppConfig.java
@@ -4,7 +4,7 @@ import java.util.ResourceBundle;
 
 public class AppConfig {
 
-	private static ResourceBundle config = ResourceBundle.getBundle("restIntegration/catalog-integration");
+	private static ResourceBundle config = ResourceBundle.getBundle("catalog-config");
 
 	public static String getUrlProductDefaultImage() {
 		return config.getString("url.default.image");
